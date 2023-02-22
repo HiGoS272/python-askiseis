68f5526fb5575718fc9e5572af1abb68443eadb9
HiGoS272 <123154045+HiGoS272@users.noreply.github.com>
2023-02-22 10:34:47 +0200

    Add files via upload

---
+++ 22.2.23.py
@@ -0,0 +1,51 @@
+# 1:
+
+# triaOnomata=["giorgos" , "vasilis", "panos"]
+
+# for i in  triaOnomata:
+#     print(i)    
+
+
+
+# 2:
+
+# a = 10640000
+# b = 6878000
+# c = 6834000
+
+# def mesosOros():
+#     z = ( a + b + c)/3
+#     print(z)
+
+# mesosOros()    
+
+
+#  3:
+# while True:
+#     z = input ("give name:")
+#     n = int(input("give money:"))
+
+#     def muFuction():
+
+#         if n <1000:
+#             print("ftwxos")
+#         elif n >= 1000 and n<2000:    
+#             print("nomral")
+#         else:
+#             print("plousios") 
+
+#     muFuction()           
+
+# 4:
+import math
+
+a = 6
+b = 8
+
+def upotinosa():
+ 
+    p = (a **2 + b**2)
+    z = math.sqrt(p)
+    print (z)
+
+upotinosa()
