2dd810b65fe1741fab5fe9a300acd5dcf3ffd489
HiGoS272 <123154045+HiGoS272@users.noreply.github.com>
2023-03-03 10:35:06 +0200

    Add files via upload
    3/03/2023
    Signature:
    Afalis Panagiotis

---
+++ Mesi Timh Me thermokrasies.py
@@ -0,0 +1,22 @@
+ #Να γράψετε πρόγραμμα το οποίο θα διαβάζει τις 3 θερμοκρασίες από μια city.Με τη βοήθεια μια Fuction θα υπολογίζεται και θα επιστρέφει την Μέση Τιμή της θερμοκρασίας. Στη συνεχεία θα φτιάξετε μια ακομά Fuction η οποία θα εμφανίζει το μήνυμα (Frozen) αν η Μέση Τιμή είναι κάτω απο 0, το μήνυμα (Hot) αν η  Μέση Τιμή είναι απο 35 βάθμους και πάνω , και στις υπόλοιπες περιπτόσεις να εμφάνιζει το μήνυμα (Normal). 
+
+
+while True:
+
+
+    a1 = int(input("Dwse 1η Thermokrasia:"))
+    a2 = int(input("Dwse 2η Thermokrasia:"))
+    a3 = int(input("Dwse 3η Thermokrasia:"))
+
+    def thermokrasia1(a1, a2, a3):
+        mesiTimi= (a1 + a2 + a3)/3
+        return  mesiTimi
+    def thermokrasia2(mesiTimi):
+        if mesiTimi < 0:
+            print("Frozen")
+        elif mesiTimi >= 35:
+            print("Hot")
+        else:
+            print("Normal")        
+    mesiTimi=thermokrasia1(a1, a2, a3)
+    thermokrasia2(mesiTimi) 
