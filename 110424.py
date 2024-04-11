e10ce43bec4e5da092c0ae184627b70b897a3f23
HiGoS272 <123154045+HiGoS272@users.noreply.github.com>
2024-04-11 09:34:32 +0300

    askisi me klimakoti

---
+++ 110424.py
@@ -0,0 +1,29 @@
+ar = raw_input("dwse ariumo kikloforias:")
+c = 0
+while ar!="*":
+
+
+    time = int(input("dwse diarkia stathmeusis:"))
+    while time >0:
+        time = int(input("dwse diarkia stathmeusis:"))
+
+    if time <=4:
+        x = 2.5*time
+    elif time > 4 and time <=7:
+        x = 2.5*4 + (time - 4)*2
+    else:
+        x=2.5*4 + 3*2 + (time - 7)*1.5
+        
+        
+    print ar,x
+
+    if time<=3:
+        c=c+1
+        
+
+
+
+
+    ar = raw_input("dwse ariumo kikloforias:")
+
+print c
