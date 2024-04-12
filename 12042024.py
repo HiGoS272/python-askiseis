153bd8fdbbc4f03f0348bad2d9e183771fba9744
HiGoS272 <123154045+HiGoS272@users.noreply.github.com>
2024-04-12 15:55:11 +0300

    Update 12042024.py

--- 12042024.py
+++ 12042024.py
@@ -3,13 +3,13 @@
 ans=1
 
 while ans!="7":
-    print "1.Πρόσθεση"
-    print "2.Αφαίρεση"
-    print "3. Πολλαπλασιασμός"
-    print "4. Διαίρεση"
-    print "5. Ακέραιο υπόλοιπο"
-    print "6. Δυνάμεις"
-    print "7. Έξοδος"
+    print "1.prosthesi"
+    print "2.aferesi"
+    print "3. pollaplasiasmos"
+    print "4. dieresi"
+    print "5. ΓΓªΓΓ±Γ΅Γ©Γ― ΓµΓ°ΓΌΓ«Γ―Γ©Γ°Γ―"
+    print "6. dinami"
+    print "7. ΒΈtelos"
     
     ans=raw_input("What would you like to do?")
     if ans!="7":
@@ -18,34 +18,34 @@ while ans!="7":
     
     #1
     if ans=="1":
-        print "\n Πρόσθεση Added"
+        print "\n prosthesi Added"
         x = ar1+ar2
         print x
     #2    
     elif ans=="2":
-        print "\n .Αφαίρεση"
+        print "\n .aferesi"
         y =  ar1-ar2
         print y
     #3    
     elif ans=="3":
-        print "\n Πολλαπλασιασμός"       
+        print "\n pollaplasiasmos"       
         z =   ar1*ar2
         print z
     #4    
     elif ans=="4":
-        print "\n Διαίρεση"
+        print "\n dieresi"
         if ar2>0:
             o = ar1/ar2
             print o
     #5            
     elif ans=="5":
-        print "\n Ακέραιο υπόλοιπο"
+        print "\n ΓΓªΓΓ±Γ΅Γ©Γ― ΓµΓ°ΓΌΓ«Γ―Γ©Γ°Γ―"
         if ar2>0:
             p = ar1%ar2
             print p
     #6        
     elif ans=="6":
-        print "\n Δυνάμεις"
+        print "\n dinami"
         k =   ar1**ar2
         print k
     #7    
